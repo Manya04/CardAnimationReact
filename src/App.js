3214f33df2acc27af73f1a72728d00b3467c7b7f
Manya04 <manyaagarwal8@gmail.com>
2021-07-23 22:11:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Demo from './Demo/Demo';
+import Scroll from './Scroll/Scroll';
+import Testim from './Testim/Testim';
+import Demo2 from './Demo2/Demo2';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Testim></Testim>
+      {/* <Demo2></Demo2> */}
+     <Demo></Demo>
+     {/* <Scroll></Scroll> */}
+    </div>
+  );
+}
+
+export default App;
